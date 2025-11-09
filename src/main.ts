04bfc8e250c6dd85becb94ebd8d0da7eccf4a9e0
diego1999dd <diego.9090dd@gmail.com>
2025-11-09 20:58:14 -0300

    Inserção do CORS na main

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,21 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const BACKEND_PORT = parseInt(process.env.PORT || '3001', 10);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
 
-  await app.listen(process.env.PORT || 3000);
+  // O backend escuta na porta de backend (ex: 3001)
+  await app.listen(BACKEND_PORT);
 }
 bootstrap();
